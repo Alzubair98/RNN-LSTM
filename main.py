abd0b37e6a30549788b0a18688c4dae68a17fcab
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-07-17 16:49:24 +0300

    read dataset

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # Part 1 - Data Preprocessing
 
 # importing the libraries
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Inserting the training set as dataframe
+
+dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
