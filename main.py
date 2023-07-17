766c25e4a22a4827fe62169bedb32c71e293b50b
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-07-17 20:10:23 +0300

    use normalisation scaling and scale the training data

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ import pandas as pd
 
 # Inserting the training set as dataframe
 
-dataset_train = pd.read_csv('Google_Stock_Price_Train.csv') #dataFrame
+dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')  # dataFrame
 training_set = dataset_train.iloc[:, 1:2].values
 
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
 
-
-
-
+sc = MinMaxScaler(feature_range=(0, 1), copy=True)
+training_set_scaled = sc.fit_transform(training_set)
