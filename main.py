35fd7e822ffb4da2ee58a9134e8d2fa30adc5df5
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-07-18 22:17:10 +0300

    clearing the code editor

--- main.py
+++ main.py
@@ -7,23 +7,22 @@ import pandas as pd
 
 # Inserting the training set as dataframe
 
-dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')  # dataFrame
+dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
 training_set = dataset_train.iloc[:, 1:2].values
 
 # Feature Scaling
 from sklearn.preprocessing import MinMaxScaler
-
-sc = MinMaxScaler(feature_range=(0, 1), copy=True)
+sc = MinMaxScaler(feature_range = (0, 1))
 training_set_scaled = sc.fit_transform(training_set)
 
 # Creating a data structure with 60 timesteps and 1 output
 
 X_train = []
-Y_train = []
+y_train = []
 for i in range(60, 1258):
-    X_train.append(training_set_scaled[i - 60:i, 0])
-    Y_train.append(training_set_scaled[i, 0])
-X_train, Y_train = np.array(X_train), np.array(Y_train)
+    X_train.append(training_set_scaled[i-60:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
 
 # Reshaping
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
@@ -40,21 +39,23 @@ from keras.layers import Dropout
 
 regressor = Sequential()
 # adding the LSTM Layer & Dropout regularisation
-regressor.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
 regressor.add(Dropout(0.2))
 # adding second LSTM
-regressor.add(LSTM(units=50, return_sequences=True))
+regressor.add(LSTM(units = 50, return_sequences = True))
 regressor.add(Dropout(0.2))
 # adding third LSTM
-regressor.add(LSTM(units=50, return_sequences=True))
+regressor.add(LSTM(units = 50, return_sequences = True))
 regressor.add(Dropout(0.2))
 # adding fourth LSTM
-regressor.add(LSTM(units=50))
+regressor.add(LSTM(units = 50))
 regressor.add(Dropout(0.2))
 
 # adding the output layer
-regressor.add(Dense(units=1))
+regressor.add(Dense(units = 1))
 # Compiling the RNN
-regressor.compile(optimizer='adam', loss='mean_squared_error')
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 
-regressor.summary()
+# regressor.summary()
+
+regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
